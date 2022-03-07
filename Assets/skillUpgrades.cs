48b11d628b5bb133ec681163f19a085e9b5b3d10
ataberkyilmaz <ataberkyilmaz@sabanciuniv.edu>
2022-03-07 17:03:21 +0300

    skill upgrades

--- Assets/skillUpgrades.cs
+++ Assets/skillUpgrades.cs
@@ -37,4 +37,79 @@ public class skillUpgrades : MonoBehaviour
             }
         }
     }
+
+    public void theFunction()
+    {
+        print("aaaa");
+    }
+    public void increaseRadius(string s)
+    {
+        foreach (var skill in skillScriptableObjects)
+        {
+            if (skill.name == s)
+            {
+                skill.level++;
+                skill.radius += skill.radius * 10 / 100;
+            }
+        }
+    }
+
+    public void increaseDamage(string s)
+    {
+        foreach (var skill in skillScriptableObjects)
+        {
+            if (skill.name == s)
+            {
+                skill.level++;
+                skill.damage += 2;
+            }
+        }
+    }
+
+    public void increaseAmount(string s)
+    {
+        foreach(var skill in skillScriptableObjects)
+        {
+            if (skill.name == s)
+            {
+                skill.level++;
+                skill.amount += 1;
+            }
+        }
+    }
+
+    public void decreaseCooldown(string s)
+    {
+        foreach (var skill in skillScriptableObjects)
+        {
+            if (skill.name == s)
+            {
+                skill.level++;
+                skill.cooldown -= skill.cooldown * 0.15f;
+            }
+        }
+    }
+
+    public void increaseSpeed(string s)
+    {
+        foreach (var skill in skillScriptableObjects)
+        {
+            if (skill.name == s)
+            {
+                skill.level++;
+                skill.speed += skill.speed * .2f;
+            }
+        }
+    }
+    public void increaseDuration(string s)
+    {
+        foreach (var skill in skillScriptableObjects)
+        {
+            if (skill.name == s)
+            {
+                skill.level++;
+                skill.duration += skill.duration * .2f;
+            }
+        }
+    }
 }
